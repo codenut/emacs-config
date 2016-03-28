6374ffb2cf745a89808b7bff8a5f7ee5c48cf761
Michael Valladolid <mikevalladolid@gmail.com>
2016-03-28 18:20:13 +0800

    init file

---
+++ init.el
@@ -0,0 +1,25 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+
+(tool-bar-mode -1)
+(setq inhibit-startup-message t)
+(setq x-select-enable-clipboard t)
+
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+(require 'pallet)
+(add-hook 'ruby-mode-hook 'robe-mode)
+
+(load-theme 'monokai t)
+
+(add-to-list 'load-path "~/.emacs.d/custom")
+(load "03auto-complete.el")
+(load "04smartparens.el")
+(load "05neotree.el")
+(load "06projectile.el")
+(load "07web-mode.el")
