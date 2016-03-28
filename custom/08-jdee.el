2f04728d468ae7f28ce143d155158523860489c8
Michael Valladolid <mikevalladolid@gmail.com>
2016-03-28 22:01:18 +0800

    dee mode

--- custom/08-jdee.el
+++ custom/08-jdee.el
@@ -1,2 +1,5 @@
 (add-to-list 'load-file "~/.emacs.d/.cask/24.4.1/elpa/jdee-20160304.536")
 (require 'jdee)
+(autoload 'jdee-mode "jde" "JDE mode" t)
+(setq auto-mode-alist
+      (append '(("\\.java\\'")) auto-mode-alist))
