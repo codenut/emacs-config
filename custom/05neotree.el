dd9d5dfa3b5600ceda9be8d7d372a958c040b31f
Michael Valladolid <mikevalladolid@gmail.com>
2016-03-28 22:09:59 +0800

    set neotree key

--- custom/05neotree.el
+++ custom/05neotree.el
@@ -1,4 +1,4 @@
 (add-to-list 'load-path
 	     "~/.emacs.d/.cask/24.5.1/elpa/neotree-20160306.730/")
 (require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
+(global-set-key "\C-x\C-b" 'neotree-toggle)
