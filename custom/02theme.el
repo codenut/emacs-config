94649b91d1180f104bd30b04cb2c16f3cdf0225e
Michael Valladolid <mikevalladolid@gmail.com>
2016-03-29 17:58:40 +0800

    Revert "custom theme"
    This reverts commit 6ec7c169b0cd3a8355dcdee945893166f4e8d596.

--- custom/02theme.el
+++
@@ -1,4 +0,0 @@
-(add-to-list 'custom-theme-load-path "~/.emacs.d/.cask/24.5.1/elpa/moe-theme-20160322.115/")
-(add-to-list 'load-path "~/.emacs.d/.cask/24.5.1/elpa/moe-theme-20160322.115/")
-(require 'moe-theme)
-(load-theme 'moe-dark t)
--- init.el
+++ init.el
@@ -32,8 +32,15 @@
 (require 'powerline)
 (powerline-moe-theme)
 
+;(require 'mo-theme)
+;(load-theme 'zenburn t)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/.cask/24.5.1/elpa/moe-theme-20160322.115/")
+(add-to-list 'load-path "~/.emacs.d/.cask/24.5.1/elpa/moe-theme-20160322.115/")
+(require 'moe-theme)
+(load-theme 'moe-dark t)
+
 (add-to-list 'load-path "~/.emacs.d/custom")
-(load "02theme.el")
 (load "03auto-complete.el")
 (load "04smartparens.el")
 (load "05neotree.el")
